e5b87f1c24d42095df5ef65f79bdc4677a85a20a
Sandeep Dwivedi <sandeep.dwivedi@hmhco.com>
2024-03-21 10:44:07 +0530

    app.js modified

--- helm-tutorial/app/app.js
+++ helm-tutorial/app/app.js
@@ -1,16 +1,24 @@
+// Importing required modules
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port);
-console.log(`App running at http://localhost:${port}`);
+// Start the server
+app.listen(port, () => {
+  console.log(`\n\x1b[36mApp running at http://localhost:${port}\x1b[0m\n`);
+});
 
+// Route for checking server health
 app.get('/health', (req, res) => {
-  res.send('OK');
-  res.status(200);
+  // Sending a health status response
+  res.status(200).send('\n\x1b[32mServer is healthy!\x1b[0m\n');
 });
 
+// Default route
 app.get('/', (req, res) => {
+  // Getting name from environment variables or using a default value
   const name = process.env.NAME || 'HMH World';
-  res.send(`Hello ${name}!`);
-});
+  // Sending a personalized greeting
+  res.send(`\n\x1b[33mHello ${name}!\x1b[0m\n`);
+}); 
+

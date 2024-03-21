75f95133819469f4de5d3a1d9caf856da8fe948b
Sandeep Dwivedi <40598729+mail2sandeepd@users.noreply.github.com>
2024-03-21 10:51:24 +0530

    Update app.js

--- helm-tutorial/app/app.js
+++ helm-tutorial/app/app.js
@@ -5,13 +5,17 @@ const port = 3000;
 
 // Start the server
 app.listen(port, () => {
-  console.log(`\n\x1b[36mApp running at http://localhost:${port}\x1b[0m\n`);
+  console.log(`\n\n\x1b[1m\x1b[35m
+  ┏━┓┏━╸╺┳╸┏━╸╺┳╸   ┏━┓┏━┓┏━╸╻ ╻
+  ┣┳┛┣╸  ┃ ┣╸  ┃    ┗━┓┣━┫┃  ┃┏┛
+  ╹┗╸┗━╸ ╹ ┗━╸ ╹    ┗━┛╹ ╹┗━╸┗┛ 
+  \x1b[0m\n\x1b[36mApp running at http://localhost:${port}\x1b[0m\n\n`);
 });
 
 // Route for checking server health
 app.get('/health', (req, res) => {
   // Sending a health status response
-  res.status(200).send('\n\x1b[32mServer is healthy!\x1b[0m\n');
+  res.send('<h1 style="color:green;">Server is healthy!</h1>');
 });
 
 // Default route
@@ -19,6 +23,5 @@ app.get('/', (req, res) => {
   // Getting name from environment variables or using a default value
   const name = process.env.NAME || 'HMH World';
   // Sending a personalized greeting
-  res.send(`\n\x1b[33mHello ${name}!\x1b[0m\n`);
+  res.send(`<h1 style="color:orange;">Hello ${name}!</h1>`);
 }); 
-
